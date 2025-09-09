b4a6d2947554b92f6ce7266e9baeead22f300e9e
João Pedro Lima <jpbenevideslima0707@gmail.com>
2025-09-08 21:17:49 -0300

    first commit

---
+++ common/disciplina/src/test/java/com/example/disciplina/DisciplinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disciplina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ common/turma/src/test/java/com/example/turma/TurmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
